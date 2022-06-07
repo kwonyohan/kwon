5416f40ce354bd22a8431fe5b56f068a7d28fd7b
chainoffreedom <kwonyh15@hanyang.ac.kr>
2022-06-08 07:31:53 +0900

    English menu change etc

--- memoreader/memoreader_starter.py
+++ memoreader/memoreader_starter.py
@@ -33,8 +33,9 @@ class mainWindow(QMainWindow, Ui_MainWindow):
         if fname[0]:
             with open(fname[0], "rb") as f:
                 data = f.read()
-                self.fdata = data
-                self.hexview.setPlainText(str(data)) 
+                self.fdata = str(data)
+                print(data.hex())
+                self.hexview.setPlainText(data.hex(' ', 1)) 
                 self.textviewer_euckr.setPlainText(data.decode('euc_kr', 'ignore'))
                 self.textviewer_utf8.setPlainText(data.decode('utf-8', 'ignore'))
                 self.textviewer_cp949.setPlainText(data.decode('cp949', 'ignore'))
@@ -43,6 +44,19 @@ class mainWindow(QMainWindow, Ui_MainWindow):
                 self.textviewer_unicode_2.setPlainText(data.decode('utf-16be', 'ignore'))
                 self.opened_file_path = fname
                 self.opened = True
+                if "efbfbdefbfbd" in data.hex():
+                    self.console.append("Found '占쏙옙(ï¿½)' in the file (efbfbdefbfbd)")
+                    msgBox = QMessageBox()
+                    if self.lan == 0:
+                        msgBox.setText("해당 파일은 '占쏙옙'이 일어난 회손된 파일일 수 있으며 \n어떤 인코딩으로도 원본을 볼 수 없음을 경고합니다.")
+                        msgBox.setWindowTitle("손상된 파일 경고")
+                        msgBox.setDetailedText("해당 파일의 HEX값에 EF BF BD EF BF BD(占쏙옙) 값이 반복되어 나타나 있다면 UTF-8로 저장하는 과정 중 오류가 발생한 손상된 문서로 어떤 방식을 통해서도 원본 복구가 불가능함을 알림니다.")
+                    else:
+                        msgBox.setText("It warns you that the file may be corrupted with an 'ï¿½' \nand that the original cannot be viewed in any encoding.")
+                        msgBox.setWindowTitle("Critical Message")
+                        msgBox.setDetailedText("If the EF BF BD EF BF BD(ï¿½) value is repeatedly displayed in the HEX value of the file, it is a damaged document that has an error during the process of saving in UTF-8 and it is not possible to recover the original through any method.")
+                    msgBox.setIcon(QMessageBox.Critical)
+                    msgBox.exec_()                    
             print(fname[0])
             print("open!!")
             self.console.append("open file : " + fname[0])
@@ -104,10 +118,10 @@ class mainWindow(QMainWindow, Ui_MainWindow):
     def verFunction(self):
         msgBox = QMessageBox()
         if self.lan == 0:
-            msgBox.setWindowTitle("memoreader 정보")
+            msgBox.setWindowTitle("Memoreader 정보")
         else :
-            msgBox.setWindowTitle("memoreader Info")
-        msgBox.setText("memoreader v0.5\n\n 2022.06.08.")
+            msgBox.setWindowTitle("Memoreader Info")
+        msgBox.setText("Memoreader v0.5\n\n 2022.06.08.")
         msgBox.setIcon(QMessageBox.Information)
         msgBox.exec_()
             
@@ -137,22 +151,26 @@ class mainWindow(QMainWindow, Ui_MainWindow):
             self.console_label.setText("상태창")
             self.console_label_2.setText("불러온 파일 HEX 값")
             self.savebutton.setText("해당 인코딩으로 다른이름으로 저장")
+            self.menu_file.setTitle("파일")
+            self.menu_help.setTitle("도움말")
             self.action_open.setText("열기")
             self.action_exit.setText("끝내기")
             self.action_help.setText("도움말 보기")
-            self.actionmemoreader.setText("memoreader 정보")
+            self.actionmemoreader.setText("Memoreader 정보")
             self.lan = 0
             
         
     def engFunction(self):
         if self.lan == 0:
-            self.console_label.setText("console")
+            self.console_label.setText("Console")
             self.console_label_2.setText("HEX value of the opened file")
             self.savebutton.setText("Save As with that encoding")
-            self.action_open.setText("open")
-            self.action_exit.setText("exit")
-            self.action_help.setText("Help")
-            self.actionmemoreader.setText("memoreader information")
+            self.menu_file.setTitle("File")
+            self.menu_help.setTitle("Help")
+            self.action_open.setText("Open")
+            self.action_exit.setText("Exit")
+            self.action_help.setText("Get Help")
+            self.actionmemoreader.setText("Memoreader information")
             self.lan = 1
         
 app = QApplication(sys.argv)
